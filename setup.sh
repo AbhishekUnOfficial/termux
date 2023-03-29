3638c150a6ee327cf7f0715bd7069298a32a41b3
Abhishek Mallick <78242724+AbhishekUnOfficial@users.noreply.github.com>
2023-03-30 00:22:06 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,10 @@
+apt update
+apt upgrade -y
+apt install ffmpeg python3 python3-pip neofetch
+pip3 install yt-dlp
+pip3 install BeautifulSoup4
+pip3 install requests
+
 mkdir bin
 cd bin
 wget https://github.com/AbhishekUnOfficial/termux/raw/main/termux-url-opener
